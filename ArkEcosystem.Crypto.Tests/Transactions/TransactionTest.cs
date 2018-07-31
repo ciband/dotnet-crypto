727450e18f3acf4c700927cf941222331de99150
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-07-31 10:45:27 -0500

    Update TransactionTest.cs

--- ArkEcosystem.Crypto.Tests/Transactions/TransactionTest.cs
+++ ArkEcosystem.Crypto.Tests/Transactions/TransactionTest.cs
@@ -83,16 +83,16 @@ namespace ArkEcosystem.Crypto.Tests.Configuration
         [TestMethod]
         public void ToJson()
         {
-            var expectedJson = @"{
-              "type": 0,
-              "amount": 200000000,
-              "fee": 10000000,
-              "recipientId": "D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib",
-              "timestamp": 41268326,
-              "asset": {},
-              "senderPublicKey": "034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192",
-              "signature": "3044022002994b30e08b58825c8c16ebf2cc693cfe706fb26571674784ead098accc89d702205b79dedc752a84504ecfe4b9e1292997f22260ee4daa102d2d9a61432d93b286",
-              "id": "da61c6cba363cc39baa0ca3f9ba2c5db81b9805045bd0b9fc58af07ad4206856"
+            var expectedJson = "{\n"
+              \"type\": 0,\n"
+              \"amount\": 200000000,\n"
+              \"fee\": 10000000,\n"
+              \"recipientId\": \"D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib\",\n"
+              \"timestamp\": 41268326,\n"
+              \"asset\": {},\n"
+              \"senderPublicKey\": \"034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192\",\n"
+              \"signature\": \"3044022002994b30e08b58825c8c16ebf2cc693cfe706fb26571674784ead098accc89d702205b79dedc752a84504ecfe4b9e1292997f22260ee4daa102d2d9a61432d93b286\",\n"
+              \"id\": \"da61c6cba363cc39baa0ca3f9ba2c5db81b9805045bd0b9fc58af07ad4206856\"\n"
             }";
             
             var fixture = TestHelper.ReadTransactionFixture("transfer", "passphrase");
